da26672b293b9f8ab172ca259a440b7c00953070
Larry Hsiao <larryhsiao@silverhetch.com>
2018-12-16 23:41:06 +0800

    update build script

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=cget/cget/cget.cmake -DCMAKE_INSTALL_PREFIX=cget ..
 cmake --build .
+ctest .
 
 lcov --directory . --capture --output-file ./code_coverage.info -rc lcov_branch_coverage=1
-ctest .
-echo ${PWD}
 genhtml code_coverage.info --branch-coverage --output-directory ./code_coverage_report/
